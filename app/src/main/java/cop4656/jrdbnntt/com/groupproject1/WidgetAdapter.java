244163a6faeea22f1ac41b919f9b29558d482c86
cristianpalencia <cristianpalencia18@gmail.com>
2017-04-04 18:45:32 -0400

    GH-26

--- app/src/main/java/cop4656/jrdbnntt/com/groupproject1/WidgetAdapter.java
+++ app/src/main/java/cop4656/jrdbnntt/com/groupproject1/WidgetAdapter.java
@@ -1,8 +1,14 @@
 package cop4656.jrdbnntt.com.groupproject1;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
+
+import java.util.ArrayList;
+
+import cop4656.jrdbnntt.com.groupproject1.provider.MyContentProvider;
+import cop4656.jrdbnntt.com.groupproject1.provider.table.Course;
 
 /**
  * Created by Cristian Palencia on 4/4/2017.
@@ -11,17 +17,45 @@ import android.widget.RemoteViewsService;
 public class WidgetAdapter implements RemoteViewsService.RemoteViewsFactory {
     Context context;
 
-    String[] list = {"Algorithms", "Android", "Java", "Programming Languages", "Python"};// Insteado of using this list use the ContentProvider to find list
+    Cursor  cursor;
+
+    String[] list = {"COP4530", "CIS4931", "COP4020"};// Insteado of using this list use the ContentProvider to find list
+
+    ArrayList<String> stringList= new ArrayList<>();
+
 
 
     public WidgetAdapter(Context context)
     {
         this.context=context;
+
+
     }
+
+
 
     @Override
     public void onCreate() {
+        cursor = context.getContentResolver().query(
+                MyContentProvider.getUriForTable(Course.TABLE_NAME),
+                new String [] {
+                        Course.COLUMN_NAME
+                },
+                null,
+                null,
+                null
+        );
+        while(cursor.moveToFirst())
+        {
+            String course = cursor.getString(0);
+            stringList.add(course);
+        }
 
+
+
+
+        list=stringList.toArray(new String[stringList.size()]);
+        cursor.close();
     }
 
     @Override
@@ -41,6 +75,9 @@ public class WidgetAdapter implements RemoteViewsService.RemoteViewsFactory {
 
     @Override
     public RemoteViews getViewAt(int i) {
+
+
+
         RemoteViews rv= new RemoteViews(context.getPackageName(),R.layout.list_item);
         rv.setTextViewText(R.id.textView, list[i]);
         return rv;
